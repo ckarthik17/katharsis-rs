71df1812ccaaff617c2c1dc8d85e43066b95579b
Błażej Krysiak <blazej.krysiak@gmail.com>
2016-02-15 23:21:44 +0100

    #20 - Added UTF-8 charset to json api media type

--- src/main/java/io/katharsis/rs/type/JsonApiMediaType.java
+++ src/main/java/io/katharsis/rs/type/JsonApiMediaType.java
@@ -1,14 +1,16 @@
 package io.katharsis.rs.type;
 
 import javax.ws.rs.core.MediaType;
+import java.nio.charset.StandardCharsets;
 
 public class JsonApiMediaType {
     /**
      * A {@code String} constant representing {@value #APPLICATION_JSON_API} media type.
      */
-    public final static String APPLICATION_JSON_API = "application/vnd.api+json";
+    public final static String APPLICATION_JSON_API = "application/vnd.api+json;charset=UTF-8";
     /**
      * A {@link MediaType} constant representing {@value #APPLICATION_JSON_API} media type.
      */
-    public final static MediaType APPLICATION_JSON_API_TYPE = new MediaType("application", "vnd.api+json");
+    public final static MediaType APPLICATION_JSON_API_TYPE = new MediaType("application", "vnd.api+json")
+        .withCharset(StandardCharsets.UTF_8.displayName());
 }
